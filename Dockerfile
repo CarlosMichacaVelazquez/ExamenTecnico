9936e3940d6a805d7262a5f7e48113e0bc030809
CarlosMichaca <67929792+CarlosMichaca@users.noreply.github.com>
2021-07-17 03:27:51 -0500

    three comit

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,13 @@ WORKDIR /app
 
 ENV FLASK_APP=main.py
 
+ENV FLASK_RUN_HOST=0.0.0.0
+
 ENV FLASK_DEBUG=1
 
 COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
-
-EXPOSE 5000
 
 COPY . .
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request, jsonify, render_template
 from sqlalchemy import create_engine, text
 import pandas as pd
@@ -7,6 +9,11 @@ engine = create_engine('postgresql+psycopg2://postgres:123456@localhost:5432/my_
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload')
 def upload_excel():
     return render_template('upload.html')
 
@@ -16,32 +23,47 @@ def upload_file():
     if request.method == 'POST':
         filename = request.files['file']
         with pd.ExcelFile(filename) as xls:
+            sheetName = str(xls.sheet_names)[2:-2]
+            html = pd.read_excel(xls).to_html()
             df = pd.read_excel(xls)
-            if df.columns.values[0] == 'Nombre':
-                df.to_sql(name='empleados', con=engine, if_exists='append', index=False)
-            elif df.columns.values[0] == 'Ciudad':
-                df.to_sql(name='lugares', con=engine, if_exists='append', index=False)
+            df.to_sql(sheetName, if_exists='replace', con=engine)
 
-    return 'Archivo cargado con éxito en la base de datos.'
+    return render_template('uploader.html', html_code=html)
 
 
-@app.route('/table/<name>/<format>', methods=['GET'])
-def get_table(name, format):
-    with engine.connect() as connection:
-        if format.upper() == 'A':
-            result = connection.execute(text('SELECT * FROM'+' '+name))
-            var = ({name: [dict(row) for row in result]})
-            return jsonify(var)
-        elif format.upper() == 'B':
-            empleados = "'empleado'"
-            nombre = "'nombre'"
-            consulta = ('SELECT json_build_object({},"Compania",'
-                        '{},(select json_agg(row_to_json((select r from( select "Nombre","Email","Fecha de ingreso") '
-                        'r))))) as empresa FROM empleados GROUP by "Compania"').format(nombre,empleados)
-            result = connection.execute(text(consulta))
-            var = ([dict(row) for row in result])
-            return jsonify(var)
+@app.route('/table', methods=['GET', 'POST'])
+def get_table():
+    if request.method == 'POST':
+        table_list = request.values['table_list']
+        format_json = request.values['formato']
+        with engine.connect() as connection:
+            if format_json == 'A':
+                result = connection.execute(f'SELECT * FROM "{table_list}"')
+                json_format_a = jsonify({table_list: [dict(row) for row in result]})
+                return json_format_a
+            elif format_json == 'B':
+                empleados = "'empleado'"
+                nombre = "'nombre'"
+                consulta = (f'SELECT json_build_object({nombre},"Compania",'
+                            f'{empleados},(select json_agg(row_to_json((select r from( select "Nombre","Email",'
+                            f'"Fecha de ingreso") '
+                            'r))))) as empresa FROM empleados GROUP by "Compania"')
+                result = connection.execute(text(consulta))
+                var = ([dict(row) for row in result])
+                return jsonify(var)
+
+
+@app.route('/consult_table', methods=['GET'])
+def consult_table():
+    with engine.connect() as conn:
+        result = conn.execute("SELECT table_name as nombre from information_schema.tables WHERE "
+                              "table_schema='public' AND "
+                              "table_type='BASE TABLE'")
+        y = []
+        for row in result:
+            y.append(row['nombre'])
+        return render_template('Consult.html', table_list=y)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
